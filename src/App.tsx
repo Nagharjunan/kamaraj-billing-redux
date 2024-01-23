7af28998b0624aa809f6e4c8023746fb079a7f30
Nagharjunan <r.nagharjunan@outlook.com>
2024-01-23 11:51:11 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,7 @@ function App() {
         <Navbar></Navbar>
         <ToastContainer
           position="top-right"
-          autoClose={5000}
+          autoClose={1000}
           hideProgressBar={false}
           newestOnTop={false}
           closeOnClick
@@ -35,7 +35,6 @@ function App() {
           pauseOnHover
           theme="dark"
         />
-        <ToastContainer />
         <div className="routes">
           <Routes>
             <Route path="/" element={<LoginComponent></LoginComponent>}></Route>
